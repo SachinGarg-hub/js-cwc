41b78ec9ed50b0ea5c75ecad0db7d35b944c8b41
Sachin garg <gargsachin809@gmail.com>
2025-10-25 04:34:09 +0530

    Add files via upload

---
+++ 01_jsBasics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name = "Sachin"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
